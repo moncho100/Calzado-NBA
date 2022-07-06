0b89756024f0f13dd5005f3bdaf8d8e5b289f4f3
Martin Zuvilivia <88513813+moncho100@users.noreply.github.com>
2022-07-06 16:03:39 -0300

    Agregando imagenes y nuevas categorias

--- js/globales.js
+++ js/globales.js
@@ -1,4 +1,4 @@
 //Variables
 const productos = [];
 const carrito   = [];
-const categorias = ["Curry","Irving","Lillard", "Giannis","James","Durant"];
+const categorias = ["Curry","Irving","Harden", "Giannis","James","George","Durant"];
